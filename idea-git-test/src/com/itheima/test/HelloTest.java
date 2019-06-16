48861c31b3a166434d9ebc0137197e35d3137d30
ningqi <1401413182@qq.com>
2019-06-16 21:44:34 +0800

    添加test到git

---
+++ idea-git-test/src/com/itheima/test/HelloTest.java
@@ -0,0 +1,9 @@
+package com.itheima.test;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        System.out.println("都铎王朝");
+        System.out.println("波旁王朝");
+        System.out.println("罗曼诺夫王朝");
+    }
+}
